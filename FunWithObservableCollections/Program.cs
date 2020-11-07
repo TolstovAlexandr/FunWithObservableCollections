5b250ac6ef9d3cc027c5f010b0103ce29598d94c
Alexandr Tolstov <SESA574650@se.com>
2020-11-07 15:45:03 +0600

    Add project files.

---
+++ FunWithObservableCollections/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace FunWithObservableCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<Person> people = new ObservableCollection<Person>()
+            {
+                new Person{FirstName = "Peter", LastName = "Murphy", Age = 52},
+                new Person{ FirstName = "Kevin", LastName = "Key", Age = 48 },
+            };
+            people.CollectionChanged += People_CollectionChanged;
+
+            people.Add(new Person { FirstName = "Alexand", LastName = "Tolstov", Age = 35 });
+
+            people.RemoveAt(1);
+        }
+
+        private static void People_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("Action for this event: {0}", e.Action);
+
+            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are the OLD items:");
+                foreach(Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Here are the NEW items:");
+                foreach (Person p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+        }
+    }
+}
